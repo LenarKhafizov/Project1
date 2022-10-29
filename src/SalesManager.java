879576f618dfafb263e226929893ed561c0e881d
Lenar Khafizov <lenarkh@mail.ru>
2022-10-29 22:36:37 +0300

    Добавление метода middle()

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,20 @@ public class SalesManager {
         }
         return max;
     }
+    public int middle() {
+        int max = sales[0];
+        int min = sales[0];
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        sum = sum - min - max;
+        return sum / (sales.length - 2);
+    }
 }
